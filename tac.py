df0499f1fd36aac3f94ac56d95d8820d6b06fcd7
Abby <78409617+abyamo@users.noreply.github.com>
2025-07-10 23:24:18 -0400

    V2 for CLI vs CLI

--- tac.py
+++ tac.py
@@ -7,7 +7,7 @@ def main():
     current_player = 1 # player 1 - X, player 2 - O
     turn_counter = 0
 
-    print("Welcome to a new round of Tic tac toe!")
+    print("Welcome to a new round of tic-tac-toe!")
 
     # max 9-turn game initiated
     while turn_counter < 9:
@@ -16,8 +16,7 @@ def main():
         printGrid(gameGrid)
 
         # gets player input for turn
-        turn_input = input(f"Player {current_player}, enter a position 
-1-9: ")
+        turn_input = input(f"Player {current_player}, enter a position 1-9: ")
         while not isValidInput(turn_input, gameGrid):
             turn_input = input(f"Invalid input! Try again: ")
         
@@ -44,26 +43,19 @@ def printGrid(gameGrid: List) -> None:
         if cell is None: display.append(idx + 1)
         else: display.append(cell)
         
-    print(f" {display[0]} | {display[1]} | {display[2]} \n---|---|--- \n 
-{display[3]} | {display[4]} | {display[5]} \n---|---|--- \n {display[6]} | 
-{display[7]} | {display[8]} \n")
+    print(f" {display[0]} | {display[1]} | {display[2]} \n---|---|--- \n {display[3]} | {display[4]} | {display[5]} \n---|---|--- \n {display[6]} | {display[7]} | {display[8]} \n")
 
 def isValidInput(turn_input, gameGrid: List) -> bool:
     try:
-        if int(turn_input) > 0 and int(turn_input) < 10 and 
-gameGrid[int(turn_input)-1] is None:
+        if int(turn_input) > 0 and int(turn_input) < 10 and gameGrid[int(turn_input)-1] is None:
             return True
         return False
     except:
         return False
 
 def winCheck(gameGrid: List) -> bool:  
-    # NOTE: For checking - instead of duplicating for X and O, check if 
-all three positions in a winning combo contain the same non-null symbol -- 
-have to do for loop thru gameGrid to see if satisfies 8 times (# of rows + 
-# of cols + 2 for diag) - more optimal way?
-    # NOTE: see if more optimal way than for loop thru all 8 winning 
-combos > bit masking??
+    # NOTE: For checking - instead of duplicating for X and O, check if all three positions in a winning combo contain the same non-null symbol -- have to do for loop thru gameGrid to see if satisfies 8 times (# of rows + # of cols + 2 for diag) - more optimal way?
+    # NOTE: see if more optimal way than for loop thru all 8 winning combos > bit masking??
 
     #     return True - if win (no player)
     # return False - if no win
